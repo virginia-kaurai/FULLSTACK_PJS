6bf64e22d0fa9ddc6813f5d2dd844bb61dd4cfa2
virginia-kaurai <virginiakaurai.com>
2026-06-22 13:20:22 +0300

    added axios

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,4 +1,16 @@
+
+import { useState, useEffect } from 'react'
+import api from './api.js'
 const Dashboard = () => {
+
+  const [profile, setProfile] = useState(null)
+
+useEffect(() => {
+  api.get('/person/')
+    .then(response => {
+      setProfile(response.data[0])
+    })
+}, [])
   return (
     <>
     <div className="flex-1 bg-slate-950 min-h-screen">
@@ -21,22 +33,24 @@ const Dashboard = () => {
 
         <div className="bg-green-500 shadow-lg rounded-lg p-6 text-center font-semibold">
           <h2>Expense</h2>
-          <p>550</p>
+          <p>{profile ? profile.profit : 'Loading...'}</p>
         </div>
 
         <div className="bg-green-500 shadow-lg rounded-lg p-6 text-center font-semibold">
           <h2>Profit</h2>
-          <p>#</p>
+          <p>{profile ? profile.profit : 'Loading...'}</p>
         </div>
 
         <div className="md:col-span-2 bg-slate-950 shadow-lg rounded-lg border border-green-500 p-6 text-center font-semibold">
           <h2 className="text-green-500">Account Balance</h2>
-          <p className="text-green-500">hello there</p>
+          <p className="text-green-500">
+  {profile ? profile.referal_code * 300 : 'Loading...'}
+</p>
         </div>
 
         <div className="md:col-span-2 bg-green-500 shadow-lg rounded-lg p-6 text-center font-semibold">
           <h2>Total Withdrawn</h2>
-          <p>hello there</p>
+          <p>{profile ? profile.Total_withdrawn : 'Loading...'}</p>
         </div>
       </div>
     </div>
@@ -62,25 +76,25 @@ const Dashboard = () => {
         <tbody className="text-green-500">
           <tr className="border-b border-green-500">
             <td className="p-3">Monday</td>
-            <td className="p-3">📺 YouTube Video</td>
+            <td className="p-3">YouTube Video</td>
             <td className="p-3">1 Video</td>
           </tr>
 
           <tr className="border-b border-green-500">
             <td className="p-3">Tuesday</td>
-            <td className="p-3">📖 Blog Post</td>
+            <td className="p-3"> Blog Post</td>
             <td className="p-3">1 Blog</td>
           </tr>
 
           <tr className="border-b border-green-500">
             <td className="p-3">Wednesday</td>
-            <td className="p-3">🎬 Instagram Reel</td>
+            <td className="p-3"> Instagram Reel</td>
             <td className="p-3">1 Reel</td>
           </tr>
 
           <tr>
             <td className="p-3">Thursday</td>
-            <td className="p-3">❓ Trivia Questions</td>
+            <td className="p-3"> Trivia Questions</td>
             <td className="p-3">5 Questions</td>
           </tr>
         </tbody>
--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://localhost:8000/api'
 })
+
+export default api
