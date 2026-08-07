14a42346da9f2ecff3aa12caf5911a9e54bffde8
virginia-kaurai <virginiakaurai.com>
2026-08-07 14:26:57 +0300

    finished on the ccustom user model and permissions

--- classroom/models.py
+++ classroom/models.py
@@ -1,11 +1,103 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
 from django.db import models
-
-
 from django.utils import timezone
+from django.conf import settings
 
+
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("role", "admin")
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
+
+        return self.create_user(
+            email=email,
+            password=password,
+            **extra_fields
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    ROLE_CHOICES = (
+        ("admin", "Admin"),
+        ("teacher", "Teacher"),
+        ("student", "Student"),
+    )
+
+    email = models.EmailField(
+        unique=True
+    )
+
+    first_name = models.CharField(
+        max_length=100
+    )
+
+    last_name = models.CharField(
+        max_length=100
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+
+    is_staff = models.BooleanField(
+        default=False
+    )
+
+    is_verified = models.BooleanField(
+        default=False
+    )
+
+    date_joined = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name"
+    ]
+
+    def __str__(self):
+        return self.email
 
 class Faculty(models.Model):
     name = models.CharField(max_length=100)
@@ -61,6 +153,12 @@ class Teacher(models.Model):
     Experience = models.IntegerField(help_text="Experience in years")
     Qualification = models.CharField(max_length=100)
     salary = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+         null=True,
+    blank=True
+    )
 
     created_at = models.DateTimeField(default=timezone.now)
 
@@ -75,6 +173,13 @@ class Student(models.Model):
     class_name = models.ForeignKey(Class, on_delete=models.CASCADE)
     Home_address = models.CharField(max_length=255)
     created_at = models.DateTimeField(default=timezone.now)
+    user = models.OneToOneField(
+            settings.AUTH_USER_MODEL,
+            on_delete=models.CASCADE,
+             null=True,
+    blank=True
+        )
+    
 
     def __str__(self):
         return self.name
